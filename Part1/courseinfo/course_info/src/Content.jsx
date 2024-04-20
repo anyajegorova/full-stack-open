44a00ae6d2712959e313921c774610ad5bde7af0
anyajegorova <anna.jerm@outlook.com>
2024-04-20 23:57:58 +0300

    step 5

--- Part1/courseinfo/course_info/src/Content.jsx
+++ Part1/courseinfo/course_info/src/Content.jsx
@@ -1,6 +1,7 @@
 import Part from './Part';
 
 const Content = (props) => {
+    console.log(props)
     return (
         props.parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises} />)
     )
--- Part1/courseinfo/course_info/src/Total.jsx
+++ Part1/courseinfo/course_info/src/Total.jsx
@@ -1,5 +1,4 @@
 const Total = (props) => {
-    console.log(props.parts);
     return (
         <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     )

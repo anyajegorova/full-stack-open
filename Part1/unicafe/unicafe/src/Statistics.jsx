30a91acdee0b06d7466b6b991d1ba5bc0a4bdc70
anyajegorova <anna.jerm@outlook.com>
2024-04-23 21:13:57 +0300

    unicafe step 2

--- Part1/unicafe/unicafe/src/Statistics.jsx
+++ Part1/unicafe/unicafe/src/Statistics.jsx
@@ -1,11 +1,17 @@
 import Feedback from './Feedback.jsx';
 
 const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad;
+    const average = (good + (-bad)) / all + '%';
+    const positive = good / all * 100 + '%';
     return (
         <>
             <Feedback text="good" count={good} />
             <Feedback text="neutral" count={neutral} />
             <Feedback text="bad" count={bad} />
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive}</p>
         </>
     )
 }

8ac93e7f9199101b50ad1449b11141d9f9033c44
anyajegorova <anna.jerm@outlook.com>
2025-12-30 21:14:21 +0200

    step 1

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,26 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>{persons.map((person) => (<p key={person.name}>{person.name}</p>))}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
